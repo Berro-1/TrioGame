0bd57816b92b385d651dbfad4b65e7043a359728
unknown <jihadhijazi977@gmail.com>
2024-07-03 22:09:33 +0300

    preload added

--- scripts/levels/level3.js
+++ scripts/levels/level3.js
@@ -5,11 +5,28 @@ class Level3 extends Phaser.Scene {
         this.player2Coins = 0;
     }
     preload(){
-
+        this.loadData();
+        this.load.image("tileset", "../../assets/Levels/tileset.png");
+        this.load.image("character", "../../assets/Images/kalkaboot.png");
+        this.load.image("background2", "../../assets/Images/chapter2-bg.jpg");
+        this.load.tilemapCSV("map3", "./assets/Levels/map3.csv");
     }
 
     create(){
-        
+
+    }
+
+    loadData() {
+        const savedData = localStorage.getItem('gameData');
+        if (savedData) {
+            const gameData = JSON.parse(savedData);
+            this.player1Coins = gameData.player1Coins;
+            this.player2Coins = gameData.player2Coins;
+        } else {
+            this.player1Coins = 0;
+            this.player2Coins = 0;
+            console.log('No saved data found, starting with default values.');
+        }
     }
 }
 
