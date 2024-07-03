cdcb8d921c98cc1c21bdb888f08e25339da6bc27
unknown <jihadhijazi977@gmail.com>
2024-07-03 21:12:27 +0300

    added base of level3 js file

---
+++ scripts/levels/level3.js
@@ -0,0 +1,16 @@
+class Level3 extends Phaser.Scene {
+    constructor() {
+        super({ key: "Level3" });
+        this.player1Coins = 0;
+        this.player2Coins = 0;
+    }
+    preload(){
+
+    }
+
+    create(){
+        
+    }
+}
+
+export default Level3;
